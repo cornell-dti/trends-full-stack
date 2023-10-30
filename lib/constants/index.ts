68e1f573e75a12fb610ab72d5808b4e0a8fb781d
Daniel Wei <weidaniel15@gmail.com>
2023-10-29 21:30:20 -0400

    Add BASE_URL_BACKEND, BASE_URL_FRONTEND

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,2 +1,4 @@
-export const BASE_URL_BACKEND = "http://localhost:8080";
-export const BASE_URL_FRONTEND = "http://localhost:5173";
+export const BASE_URL_BACKEND =
+    process.env.BASE_URL_BACKEND ?? "http://localhost:8080";
+export const BASE_URL_FRONTEND =
+    process.env.BASE_URL_FRONTEND ?? "http://localhost:5173";
